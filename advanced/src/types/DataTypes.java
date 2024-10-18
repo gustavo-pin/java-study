2e25c1922ed90f8d7a6253d756aeaeeced6e3c63
Gustavo Martins <gustavomartins.morais@outlook.com>
2024-10-18 10:18:52 -0300

    comments of non-primitives data types

--- advanced/src/types/DataTypes.java
+++ advanced/src/types/DataTypes.java
@@ -2,6 +2,18 @@ package types;
 
 public class DataTypes {
     public static void main(String[] args) {
+        // non-primitives data types is the type that is not defined by the language it-self
+
+        /*
+        * Like:
+        *
+        * - Classes
+        * - Objects
+        * - Strings
+        * - Arrays
+        * - Interfaces
+        * */
+
 
     }
 }

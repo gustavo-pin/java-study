232eeb9cb4ce2ce3bccbe1bc574e57762c1192d3
Gustavo Martins <gustavomartins.morais@outlook.com>
2024-10-18 10:19:50 -0300

    removing main function in DataTypes class

--- advanced/src/types/DataTypes.java
+++ advanced/src/types/DataTypes.java
@@ -1,19 +1,15 @@
 package types;
 
 public class DataTypes {
-    public static void main(String[] args) {
-        // non-primitives data types is the type that is not defined by the language it-self
+    // non-primitives data types is the type that is not defined by the language it-self
 
-        /*
-        * Like:
-        *
-        * - Classes
-        * - Objects
-        * - Strings
-        * - Arrays
-        * - Interfaces
-        * */
-
-
-    }
+    /*
+    * Like:
+    *
+    * - Classes
+    * - Objects
+    * - Strings
+    * - Arrays
+    * - Interfaces
+    * */
 }
